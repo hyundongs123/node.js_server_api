ec5a5fa09c40deabf33f38d57235089f5657bc3c
hyundong <hyundong456@gmail.com>
2024-09-23 18:44:40 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/sound/:name', (req, res) => {
+    const {name} = req.params
+    
+    if(name == "dog"){
+        res.json({"sound":'멍멍'})
+    }else if (name == "cat"){
+        res.json({"sound":"야옹"})
+    }else if (name == "pig"){
+        res.json({"sound":"꿀꿀"})
+    }else {
+        res.json({"sound":"알수없음"})
+    }
+
+})
+
+app.get('/cat', (req, res) => {
+  res.send('고양이!')
+})
+
+
+
+app.listen(port,()=>{
+    console.log('running node.js')
+})
+
+
+
+
+
+
+
+
+
+
